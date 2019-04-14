cc3694f7d634b01838e52e0bbc1cff04a9cfbf62
Ismael Sayas Arrieta <isayas@bancolombia.com.co>
2019-04-14 00:53:11 -0500

    Sheld created

--- src/modules/marketplace/components/marketPlaceMain/marketPlaceMain.jsx
+++ src/modules/marketplace/components/marketPlaceMain/marketPlaceMain.jsx
@@ -2,12 +2,21 @@ import React from "react";
 
 import { RouteManager } from "./../../../../core/components/routeManager/RouteManager";
 import { marketPlace_routes } from "./../../../../config/routes";
+import { HeaderComponent } from "../../../../shared/components/header/header_component";
+import { FooterComponent } from "../../../../shared/components/footer/footer_component";
 
 export const MarketPlaceMain = () => {
   return (
-    <div>
-      Main work. Child route:
-      <RouteManager routes={marketPlace_routes} />
+    <div className="marketplace">
+      <header className="marketplace__header">
+        <HeaderComponent />
+      </header>
+      <section className="marketplace__page-box">
+        <RouteManager routes={marketPlace_routes} />
+      </section>
+      <footer className="marketplace__footer">
+        <FooterComponent />
+      </footer>
     </div>
   );
 };
--- src/shared/components/header/header_component.jsx
+++ src/shared/components/header/header_component.jsx
@@ -1,11 +1,20 @@
 import React from "react";
+import "./header_component.scss";
+import logo from "./../../../assets/img/handy-logo.png";
+import { ReactComponent as CartIcon } from "./../../../assets/icons/cart.svg";
 
-export const HeaderComponent = () => {
+export const HeaderComponent = props => {
+  const count = 0;
   return (
     <div className="header">
       <div className="header__content">
-        <div className="header__title-box" />
-        <div className="header__sub-title-box" />
+        <div className="header__logo-box">
+          <img src={logo} alt="logo" />
+        </div>
+        <div className="header__cart-box">
+          <span className="header__cart-count"> {count} </span>
+          <CartIcon className="header__cart" />
+        </div>
       </div>
     </div>
   );
